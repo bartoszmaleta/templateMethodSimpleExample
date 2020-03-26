ebbb6016f0212a59ef6cfa00e46fa5d5484aac60
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-03-26 10:10:04 +0100

    Add main prints

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        Network network = null;
+        String login, password, message;
+
+        System.out.println("Provide login: ");
+        login = scanner.nextLine();
+
+        System.out.println("Provide password: ");
+        password = scanner.nextLine();
+
+        System.out.println("Provide message; ");
+        message = scanner.nextLine();
+
+        System.out.println("Where You want to post message?\n 1. Twitter\n 2. Facebook");
+        switch (scanner.nextInt()) {
+            case 1:
+                network = new Twiiter(login, password);
+                break;
+            case 2:
+                network = new Facebook(login, password);
+                break;
+        }
+
+        network.post(message);
     }
 }
